1ba621c2b614310265f06e2b20bf39615b1c91b2
Thomas Howard <thomashoward206@gmail.com>
2022-05-08 11:54:59 -0700

    1.1

---
+++ Functions.py
@@ -0,0 +1,10 @@
+
+calculation_to_units = 24
+name_of_unit = "hours"
+
+
+print(f"35 days are {35 * calculation_to_units}  {name_of_unit}")
+print(f"50 days are {50 * calculation_to_units}  {name_of_unit}")
+print(f"75 days are {75 * calculation_to_units}  {name_of_unit}")
+print(f"100 days are {100 * calculation_to_units}  {name_of_unit}")
+
---
+++ main.py
@@ -0,0 +1,6 @@
+print("Hello Thomas")
+
+#strings and numbers datatypes
+# print(20*24*60)
+# print("20 days are " + str(20*24*60) + " minutes")
+print(f"In 20 days there are {20*24*60}  minutes")
