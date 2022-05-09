2d5b3c442ec3664bca19364207c2b8134c98b089
Thomas Howard <thomashoward206@gmail.com>
2022-05-08 19:34:29 -0700

    1.5

--- Lists.py
+++ Lists.py
@@ -6,3 +6,6 @@ print(bicycles[3])
 
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles[-1])  #prints last indexed value
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print("My first bike was a " + bicycles[1].title())
