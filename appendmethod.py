f166ec448822e5731fced11da92d61598516782e
Thomas Howard <thomashoward206@gmail.com>
2022-05-08 19:43:22 -0700

    1.7

--- appendmethod.py
+++ appendmethod.py
@@ -3,3 +3,11 @@
 motorcycles = ['Yamaha', 'harley','suzuki' ]
 motorcycles.append('ducati')
 print(motorcycles)
+
+#start with an empty list
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
---
+++ insert_delFunction.py
@@ -0,0 +1,8 @@
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(1, 'ducati')
+print(motorcycles)
+
+del motorcycles[2]
+print(motorcycles)
