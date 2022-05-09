f8d300f91f137f3ae148fc86710e7282c1860534
Thomas Howard <thomashoward206@gmail.com>
2022-05-08 19:32:32 -0700

    1.3

---
+++ Lists.py
@@ -0,0 +1,8 @@
+
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[1])
+print(bicycles[3])
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[-1])  #prints last indexed value
