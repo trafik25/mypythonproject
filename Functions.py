1fa2202ccf6fbed504ce63d5849ce1c5caba2a38
Thomas Howard <thomashoward206@gmail.com>
2022-05-10 10:28:20 -0700

    1.0

--- Functions.py
+++ Functions.py
@@ -4,14 +4,12 @@ calculation_to_units = 24
 name_of_unit = "hours"
 
 
-def days_to_units(num_of_days, custom_message):
-    print(f"{num_of_days} days are {num_of_days * calculation_to_units}  {name_of_unit}")
-    print(custom_message)
+def days_to_units(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calculation_to_units}  {name_of_unit}"
 
 
-days_to_units(20, "Message")
-days_to_units(35, "Message")
-
+my_var = days_to_units(20)
+print(my_var)
 
 
 
