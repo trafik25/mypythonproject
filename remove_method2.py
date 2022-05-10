cb41e16f00f5269c6d2d6612e5b9a5cc0d8a583e
Thomas Howard <thomashoward206@gmail.com>
2022-05-10 10:58:35 -0700

    1.0

---
+++ remove_method2.py
@@ -0,0 +1,8 @@
+####### new Line
+cars = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(cars)
+
+too_expensive = 'ducati'
+cars.remove(too_expensive)
+print(cars)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
