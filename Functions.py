596bb6ee43318d3a3310e87aa1ac4cefd9e62649
Thomas Howard <thomashoward206@gmail.com>
2022-05-10 10:38:17 -0700

    1.0

--- Functions.py
+++ Functions.py
@@ -1,15 +1,15 @@
 
-
 calculation_to_units = 24
 name_of_unit = "hours"
-
 
 def days_to_units(num_of_days):
     return f"{num_of_days} days are {num_of_days * calculation_to_units}  {name_of_unit}"
 
+user_input = input("Hey user enter a number of days and I will convert it to hours\n")
+user_input_number = int(user_input)
 
-my_var = days_to_units(20)
-print(my_var)
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
 
 
 
