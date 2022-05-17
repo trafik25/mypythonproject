72f4cc9bf49e5a1c981b911bc1e11d20bb3aab8b
Thomas Howard <thomashoward206@gmail.com>
2022-05-16 20:04:43 -0700

    1.0

---
+++ Chapter4/Looping_through_lists.py
@@ -0,0 +1,11 @@
+
+#page 87
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    #print(magician)
+    print("Hey " + magician.title() + " that's a great trick!")
+
+print("Hey Everyone, thanks for the show tonight")
+
+
+####Page 92
